9b1f3e9a8f51741828587e585b1f8f95d9ada32f
Maxime  P <contact@maxime-p.dev>
2022-11-20 00:29:13 +0100

    adding beforeunload script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import type { AppProps } from 'next/app'
 import {Provider} from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react'
+import Script from 'next/script'
+
 import {store, persistor} from "@Stores"
+
 import '@Styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Provider store={store}>
     <PersistGate persistor={persistor}>
       <Component {...pageProps} />
+      <Script>
+        {`window.addEventListener('beforeunload', (e) => {e.preventDefault();e.returnValue = '';})`}
+      </Script>
     </PersistGate>
   </Provider>
 }
